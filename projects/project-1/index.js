fa2d983d244833aa55b79828d733b2d9100dfa73
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-08-22 22:23:33 -0700

    Add files via upload

---
+++ projects/project-1/index.js
@@ -0,0 +1,13 @@
+document.getElementById("headline").textContent = "Delicious Piddza!";
+document.getElementById("paragraph").textContent = "The best piddza in town!";
+//comment
+/*
+comment
+*/
+
+// 1. declaration let x;
+// 2. assignment x = 5;
+
+let x;
+x = 5;
+console.log(x);
